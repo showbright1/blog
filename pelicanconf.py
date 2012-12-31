4133ac1976a76ddc146d462e8c36b8f112b9c812
Michael Martinez <machinedesigner@gmail.com>
2012-12-31 15:20:38 -0700

    added a few org-exports to notes and tweaks to css

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,9 @@ CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 # Template pages are custom pages make a jinja2 template file
 # Then make a path pointing from it to the destination like so
 # =============
-TEMPLATE_PAGES = {'pages/math-notes.html': 'notes/math-notes.html'}
+TEMPLATE_PAGES = {'pages/math-notes.html': 'notes/math-notes.html',
+                  'pages/virt-machines.html': 'notes/virt-machines.html',
+                  'pages/pythonemacs.html': 'notes/pythonemacs.html'}
 
 #=============
 # Other
