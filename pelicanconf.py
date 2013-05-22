515d588b6227466c1f1a8e230af646c1c9a5c0a8
Michael Martinez <machinedesigner@gmail.com>
2013-05-22 15:13:50 -0700

    adding twitter cards

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,8 +11,8 @@ SITEURL = 'http://michaelmartinez.in'
 GITHUB_URL = 'http://github.com/MichaelMartinez'
 TIMEZONE = 'America/Phoenix'
 DATE_FORMATS = {
-    'en': ('en_US','%B %Y')
-    }
+    'en': ('en_US', '%B %Y')
+}
 DEFAULT_DATE_FORMAT = ('%B %Y')
 
 # =============
@@ -34,12 +34,22 @@ CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 TEMPLATE_PAGES = {}
 
 #=============
+# Twitter Card
+#=============
+# https://dev.twitter.com/cards
+TWITTER_CARD_USE = (True) #| False
+#TWITTER_CARD_SITE = '@website' # The site's Twitter handle like @my_blog
+#TWITTER_CARD_SITE_ID = 123456 # The site's Twitter ID
+TWITTER_CARD_CREATOR = '@monkmartinez'  # Your twitter handle like @yourtwitname
+#TWITTER_CARD_CREATOR_ID = 56789 # The site creator's id
+
+#=============
 # Other
 #=============
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('iPython', 'http://ipython.org/'),)
+LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+         ('Python.org', 'http://python.org'),
+         ('Jinja2', 'http://jinja.pocoo.org'),
+         ('iPython', 'http://ipython.org/'),)
 FILES_TO_COPY = [('CNAME', 'CNAME')]
 STATIC_PATHS = ['images', 'ipynb']
 #=============
@@ -47,3 +57,9 @@ STATIC_PATHS = ['images', 'ipynb']
 #=============
 DEFAULT_PAGINATION = 5
 THEME = "bootstrap-pelican"
+
+#=============
+# Plugins
+#=============
+PLUGIN_PATH = '/Users/michaelmartinez/Documents/code/python/twitter_card'
+PLUGINS = ['twitter_card']
